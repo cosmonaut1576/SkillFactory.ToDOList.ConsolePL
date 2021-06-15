3fb64c5745e08b64ee2a6db4ec7d379c604857c0
Саляхов Сергей <salyakhovsergei@gmail.com>
2021-06-15 21:16:03 +1000

    SergeyS

---
+++ SkillFactory.ToDOList.Entities/TaskStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace SkillFactory.ToDOList.Entities
+{
+    public static class TaskStatus
+    {
+        public static readonly string taskInProcess = "В процессе";
+        public static readonly string taskDone = "Завершено";
+    }
+}
